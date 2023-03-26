6bbb884b4a9117c61f198cc72bcd68916413cbde
sunny <dance@kakao.com>
2023-03-26 16:10:16 +0900

    🚀feat: 다양한형태의 obj들 추가, 반응형 추가

--- src/index.js
+++ src/index.js
@@ -7,28 +7,68 @@ if (WEBGL.isWebGLAvailable()) {
 
   //장면생성
   const scene = new THREE.Scene();
-  scene.background = new THREE.Color(0xBB2649);
+  // scene.background = new THREE.Color(0xBB2649);
 
   //카메라생성
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-  const canvas = document.querySelector('#sunny-first');
+  camera.position.z = 3;
+  // const canvas = document.querySelector('#sunny-first');
 
   //렌더러생성
-  const renderer = new THREE.WebGLRenderer({canvas});
+  const renderer = new THREE.WebGLRenderer({
+    alpha: true,
+    antialias: true,
+  });
   renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
   
-  
+  //메쉬생성
+  const geometry01 = new THREE.BoxGeometry(0.3, 0.6, 0.1);
+  const material01 = new THREE.MeshStandardMaterial({
+    color: 0xAB88ff,
+  });
+  const obj01 = new THREE.Mesh(geometry01, material01);
+  scene.add(obj01);
+
+
+const geometry02 = new THREE.ConeGeometry(0.3, 0.6, 0.1);
+const obj02 = new THREE.Mesh(geometry02, material01);
+obj02.position.x = -1;
+scene.add(obj02);
+
+
+const geometry03 = new THREE.IcosahedronGeometry(0.5, 0);
+const obj03 = new THREE.Mesh(geometry03, material01);
+obj03.position.x = +1;
+scene.add(obj03);
+
+
 
   function render(time) {
     time *= 0.001;  // convert time to seconds
-    // cube.rotation.x = time;
-    // cube.rotation.y = time;
+    
+    obj01.rotation.x = time;
+    obj01.rotation.y = time;
+
+
+    obj02.rotation.x = time;
+    obj02.rotation.y = time;
+
+    obj03.rotation.x = time;
+    obj03.rotation.y = time;
 
     renderer.render(scene, camera);
     requestAnimationFrame(render);
   }
 
   requestAnimationFrame(render);
+
+  function onWindowResize(){
+    camera.aspect = window. innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+  window.addEventListener('resize', onWindowResize);
 
 
 
