c12fa6964bbd7a45730919301eae1abccd644e5a
sunny <dance@kakao.com>
2023-03-26 23:05:40 +0900

    🚀feat: material 추가

--- src/index.js
+++ src/index.js
@@ -21,41 +21,87 @@ if (WEBGL.isWebGLAvailable()) {
   });
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
+
+
+
+
+  //라이트생성
+  const pointLight = new THREE.PointLight(0xffffff, 1);
+  pointLight.position.set(0, 2, 12);
+  scene.add(pointLight);
   
+  //텍스쳐생성
+  const textureLoader = new THREE.TextureLoader()
+  const textureBaseColor = textureLoader.load('../static/img/brick_wall_04_diff_4k.png');
+  const textureNormalMap = textureLoader.load('../static/img/brick_wall_04_nor_dx_4k.png');
+  const textureHeightMap = textureLoader.load('../static/img/brick_wall_04_disp_4k.png');
+  const textureRoughnessMap = textureLoader.load('../static/img/brick_wall_04_rough_4k.png');
+
+
+
+
+
   //메쉬생성
-  const geometry01 = new THREE.BoxGeometry(0.3, 0.6, 0.1);
+  const geometry01 = new THREE.SphereGeometry(0.4, 0.6, 0.1);
   const material01 = new THREE.MeshStandardMaterial({
-    color: 0xAB88ff,
+    map: textureBaseColor,
   });
+  // material01.wireframe = true;
   const obj01 = new THREE.Mesh(geometry01, material01);
+  obj01.position.x = -1;
   scene.add(obj01);
 
 
-const geometry02 = new THREE.ConeGeometry(0.3, 0.6, 0.1);
-const obj02 = new THREE.Mesh(geometry02, material01);
-obj02.position.x = -1;
+const geometry02 = new THREE.SphereGeometry(0.4, 0.6, 0.1);
+const material02 = new THREE.MeshStandardMaterial({
+  map: textureBaseColor,
+  normalMap: textureNormalMap,
+});
+const obj02 = new THREE.Mesh(geometry02, material02);
 scene.add(obj02);
 
 
-const geometry03 = new THREE.IcosahedronGeometry(0.5, 0);
-const obj03 = new THREE.Mesh(geometry03, material01);
+const geometry03 = new THREE.SphereGeometry(0.3, 0.6, 0.1);
+const material03 = new THREE.MeshStandardMaterial({
+  map: textureBaseColor,
+  normalMap: textureNormalMap,
+  displacementMap: textureHeightMap,
+  displacementScale: 0.2,
+});
+const obj03 = new THREE.Mesh(geometry03, material03);
 obj03.position.x = +1;
 scene.add(obj03);
+
+
+const geometry04 = new THREE.SphereGeometry(0.3, 0.6, 0.1);
+const material04 = new THREE.MeshStandardMaterial({
+  map: textureBaseColor,
+  normalMap: textureNormalMap,
+  displacementMap: textureHeightMap,
+  displacementScale: 0.2,
+  roughnessMap: textureRoughnessMap,
+});
+const obj04 = new THREE.Mesh(geometry04, material04);
+obj04.position.x = +2;
+scene.add(obj04);
 
 
 
   function render(time) {
     time *= 0.001;  // convert time to seconds
     
-    obj01.rotation.x = time;
-    obj01.rotation.y = time;
+    // obj01.rotation.x = time;
+    // obj01.rotation.y = time;
 
 
-    obj02.rotation.x = time;
-    obj02.rotation.y = time;
+    // obj02.rotation.x = time;
+    // obj02.rotation.y = time;
 
-    obj03.rotation.x = time;
-    obj03.rotation.y = time;
+    // obj03.rotation.x = time;
+    // obj03.rotation.y = time;
+
+    // obj04.rotation.x = time;
+    // obj04.rotation.y = time;
 
     renderer.render(scene, camera);
     requestAnimationFrame(render);
